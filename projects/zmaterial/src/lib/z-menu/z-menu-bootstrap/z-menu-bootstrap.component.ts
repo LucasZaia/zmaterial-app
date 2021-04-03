b2eb5b9f610bc4b673ba8d7c930baf38f0bf71ba
Lucas Zaia <lucas.zaia30@gmail.com>
2021-04-03 10:53:20 -0300

    menu boootstrap criado

--- projects/zmaterial/src/lib/z-menu/z-menu-bootstrap/z-menu-bootstrap.component.ts
+++ projects/zmaterial/src/lib/z-menu/z-menu-bootstrap/z-menu-bootstrap.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'z-menu-bootstrap',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ZMenuBootstrapComponent implements OnInit {
+
+    @Input() titleProject: string;
 
     constructor() {}
 
